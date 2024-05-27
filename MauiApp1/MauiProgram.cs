76935228abd217a6e519b373fdd9c0f7b4fc9dea
mattia fiorio <mattia.fiorio@akqa.com>
2024-05-27 13:47:28 +0200

    first commit

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using MauiApp1.ViewModel;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            // Register page and viewmodel as singleton. They get created for us
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+            Text = "";
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]  
+        void Add()
+        {
+            if (Text.Length <= 0) return;
+            Items.Add(Text);
+            // Add item
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+            {
+                Items.Remove(s);
+            }
+        }
+    }
+}
